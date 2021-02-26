90fc3dcfae2044d065d786caf134396502941ecb
LAPTOP-FJTG3P2R\Prenesh.Krishnan <pranesh.krishnan@skymind.my>
2021-02-26 15:43:47 +0800

    Initial commit

---
+++ src/main/java/ai/penjana/firstprogram.java
@@ -0,0 +1,17 @@
+package ai.penjana;
+
+public class firstprogram {
+    public static void main(String[] args) {
+        System.out.println("first program run successfully");
+    }
+
+    //add two numbers
+    /*
+    asdadasd
+    adsad
+
+    adad
+     */
+
+
+}
---
+++ src/main/java/ai/penjana/helloworld.java
@@ -0,0 +1,8 @@
+package ai.penjana;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+
+    }
+}
